f082a7c6d8db1607ef20ee664744392a5280c9a4
luka <luka.jevremovic@swisscom.com>
2026-05-28 11:10:34 +0200

    order Struktur verbessert

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import MainNav from "@/components/navigation/MainNav";
+import MainNav from "@/components/navigation/mainNav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
