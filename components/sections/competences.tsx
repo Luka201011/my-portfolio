af5bbef7303b0abad7cdfe40dd21d89bcf9cc425
luka <luka.jevremovic@swisscom.com>
2026-06-04 15:21:32 +0200

    contact fertiggestellt aber ohne funktionen und button

--- components/sections/competences.tsx
+++ components/sections/competences.tsx
@@ -5,7 +5,7 @@ export default function CompetencesSection() {
         <h2 className="center-Text title">Kompetenzen</h2>
       </div>
       <div className="center marginT-4 paddingL-3 paddingR-3 flex-col">
-        <div className="card-body center-Text flex-row center">
+        <div className="card-body-competences center-Text flex-row center">
           <p className="text-competences">HTML</p>
           <img
             src="/klicken.png"
@@ -13,19 +13,19 @@ export default function CompetencesSection() {
             className="klicken-icon"
           />
         </div>
-        <div className="card-body center-Text marginT-4 ">
+        <div className="card-body-competences center-Text center marginT-4 ">
           <p className="text-competences">CSS</p>
         </div>
-        <div className="card-body center-Text marginT-4">
+        <div className="card-body-competences center-Text center marginT-4">
           <p className="text-competences">JavaScript</p>
         </div>
-        <div className="card-body center-Text marginT-4">
+        <div className="card-body-competences center-Text center marginT-4">
           <p className="text-competences">Angular</p>
         </div>
-        <div className="card-body center-Text marginT-4">
+        <div className="card-body-competences center-Text center marginT-4">
           <p className="text-competences">TypeScript</p>
         </div>
-        <div className="card-body center-Text marginT-4">
+        <div className="card-body-competences center-Text center marginT-4">
           <p className="text-competences">SDX</p>
         </div>
       </div>
--- components/sections/contact.tsx
+++ components/sections/contact.tsx
@@ -4,6 +4,137 @@ export default function ContactSection() {
       <div>
         <h2 className="center-Text title">Kontakt</h2>
       </div>
+      <div className="center marginT-4 paddingL-3 paddingR-3 flex-col">
+        <div className="card-body-contact center-Text center flex-col">
+          <div>
+            <h2 className="title-contact">Aktueller Arbeitsort</h2>
+          </div>
+          <div className="center marginT-2" style={{ width: "100%" }}>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2724.345366273858!2d7.470208374222856!3d46.975601671081066!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478e3a00d329ec5b%3A0x523f3d7992284a57!2sBusiness%20Park%2C%20Ittigen%20Business%20Park%2C%20Ey%2010%2C%203063%20Ittigen!5e1!3m2!1sde!2sch!4v1780560104359!5m2!1sde!2sch"
+              width="100%"
+              height="200px"
+              allowFullScreen
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+        </div>
+        <div className="card-body-contact center flex-col marginT-4">
+          <div>
+            <h2 className="title-contact center-Text">Kontaktdaten</h2>
+          </div>
+          <div className="text-field left marginT-2 flex-col">
+            <div className="flex-row gap-4">
+              <div>
+                <img
+                  src="/ausweis.png"
+                  alt="Person Icon"
+                  className="contact-icon"
+                />
+              </div>
+              <div>
+                <p>Luka Jevremovic</p>
+              </div>
+            </div>
+            <div className="flex-row gap-4 marginT-1">
+              <div>
+                <img
+                  src="/anruf.png"
+                  alt="Telefon Icon"
+                  className="contact-icon"
+                />
+              </div>
+              <div>
+                <p>079 412 94 62</p>
+              </div>
+            </div>
+            <div className="flex-row gap-4 marginT-1">
+              <div>
+                <img src="/mail.png" alt="Mail Icon" className="contact-icon" />
+              </div>
+              <div>
+                <a href="mailto:luka.jevremovic@swisscom.com">
+                  luka.jevremovic@swisscom.com
+                </a>
+              </div>
+            </div>
+            <div className="flex-row gap-4 marginT-1">
+              <div>
+                <img
+                  src="/biografie.png"
+                  alt="Computer Icon Intranet Swisscom"
+                  className="contact-icon"
+                />
+              </div>
+              <div>
+                <a
+                  href="https://neli.swisscom.com/profile/%5Bme%5D/detail"
+                  target="_blank"
+                >
+                  Intranet
+                </a>
+              </div>
+            </div>
+            <div className="flex-row gap-4 marginT-1">
+              <div>
+                <img
+                  src="/linkedin.png"
+                  alt="LinkedIn Icon"
+                  className="contact-icon"
+                />
+              </div>
+              <div>
+                <a
+                  href="https://www.linkedin.com/in/luka-jevremovic-3994a5379/"
+                  target="_blank"
+                >
+                  LinkedIn
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="card-body-contact center flex-col marginT-4">
+          <div>
+            <h2 className="title-contact center-Text">Rand Informationen</h2>
+          </div>
+          <div className="text-field left marginT-2 flex-col">
+            <div className="flex-row gap-4">
+              <div>
+                <p>LB: Martin Näf</p>
+              </div>
+            </div>
+            <div className="flex-row gap-4 marginT-1">
+              <div>
+                <p>Host: Petre Tudor</p>
+              </div>
+            </div>
+            <div className="flex-row gap-4 marginT-1">
+              <div>
+                <p>Schultage: Montag, Dienstag</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer className="footer marginT-4">
+        <div className="center-Text">
+          <a
+            href="./Lebenslauf_von_Luka_Jevremovic.pdf"
+            target="_blank"
+            id="button-lebenslauf"
+          >
+            Lebenslauf Herunterladen
+          </a>
+        </div>
+
+        <div className="pfeil-rechts">
+          <a href="#Navigation">
+            <img src="/pfeil-nach-oben.png" alt="Nach Oben" />
+          </a>
+        </div>
+      </footer>
     </section>
   );
 }
