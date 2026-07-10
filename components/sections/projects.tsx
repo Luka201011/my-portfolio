81fdb5b3f8eb63e34fe7b8093e80c794d6017950
luka <luka.jevremovic@swisscom.com>
2026-07-10 15:32:03 +0200

    fix flip bei denn cards hat nicht funktioniert

--- components/sections/projects.tsx
+++ components/sections/projects.tsx
@@ -1,13 +1,18 @@
 "use client";
 import { useState } from "react";
-import { Trans } from "@lingui/react/macro";
+import { Trans } from "@lingui/macro";
+
 export default function ProjectsSection() {
   const [activeCard, setActiveCard] = useState<string | null>(null);
 
   const handleFlip = (cardId: string) => {
-    if (activeCard === cardId) setActiveCard(null);
-    else setActiveCard(cardId);
+    if (activeCard === cardId) {
+      setActiveCard(null);
+    } else {
+      setActiveCard(cardId);
+    }
   };
+
   return (
     <section id="projects" className="mt-30">
       <div>
@@ -16,7 +21,8 @@ export default function ProjectsSection() {
         </h2>
       </div>
       <div className="flex justify-center items-center mt-10 pl-7 pr-7 flex-col xl:grid xl:grid-cols-2 gap-8">
-        <div className="card-body bg-card-bg-white text-center p-10">
+        {/* Erste Karte: First Steps */}
+        <div className="card-body-project bg-card-bg-white text-center mt-10 p-10 h-[450px] xl:h-[320px] md:h-[320px] flex flex-col items-center justify-center">
           <div>
             <h2 className="text-lg font-bold">First Steps 2025 NEX-14</h2>
           </div>
@@ -27,7 +33,7 @@ export default function ProjectsSection() {
             </p>
           </div>
           <div>
-            <p className="mt-2">
+            <p className="mt-2 text-sm md:text-base">
               <Trans>
                 In den First Steps wurden mir die grundlegenden Kenntnisse und
                 Erwartungen vermittelt. Ich lernte die wichtigsten Tools kennen
@@ -41,12 +47,13 @@ export default function ProjectsSection() {
           </div>
         </div>
 
+        {/* Zweite Karte: Team Minion */}
         <div
-          className={`card-body-project text-center mt-10 p-10 h-[450px] xl:h-[320px] md:h-[320] ${activeCard === "minion" ? "flipped" : ""}`}
+          className={`card-body-project text-center mt-10 h-[450px] xl:h-[320px] md:h-[320px] ${activeCard === "minion" ? "flipped" : ""}`}
         >
           <div className="front-card-project bg-card-bg-white flex-col">
             <div>
-              <h2 className="text-lg font-bold">IT-Onboarding Team Minion</h2>
+              <h2 className="text-lg font-bold">IT Onboarding Team Minion</h2>
             </div>
             <div className="title-projects-line bg-second"></div>
             <div>
@@ -55,7 +62,7 @@ export default function ProjectsSection() {
               </p>
             </div>
             <div>
-              <p className="mt-2">
+              <p className="mt-2 text-sm md:text-base">
                 <Trans>
                   Das IT-Onboarding-Projekt hat mir erste technische Grundlagen
                   der Webentwicklung, darunter HTML, CSS, JavaScript, TypeScript
@@ -67,13 +74,14 @@ export default function ProjectsSection() {
                 </Trans>
               </p>
               <button
-                className="btn bg-second hover:bg-btn-hover text-white justify-self-center mt-8"
+                className="btn bg-second hover:bg-btn-hover text-white justify-self-center mt-4"
                 onClick={() => handleFlip("minion")}
               >
                 <Trans>Mehr Infos</Trans>
               </button>
             </div>
           </div>
+
           <div className="back-card-project flex-col bg-card-bg-white">
             <div>
               <h2 className="text-lg font-bold">
@@ -81,57 +89,49 @@ export default function ProjectsSection() {
               </h2>
             </div>
             <div className="title-projects-line bg-second"></div>
-            <div className="m-2">
-              <p className="mb-2">
+            <div className="m-2 text-sm">
+              <p className="mb-2 font-bold">
                 <Trans>Das konnte ich alles umsetzten:</Trans>
               </p>
-              <div>
-                <p>
-                  <strong>Portfolio-Website:</strong>
-                  <Trans>Ich habe eine Portfolio Website erstellt.</Trans>
-                </p>
-              </div>
-              <div>
-                <p>
-                  <Trans>
-                    <strong>Taschenrechner:</strong>
-                    In einer Gruppenarbeit entwickelte ich mit meinen
-                    Teamkollegen einen einfachen Taschenrechner als
-                    Web-Applikation. Dabei übernahm ich die Implementierung der
-                    Grundfunktionen wie Addition, Subtraktion, Multiplikation
-                    und Division.
-                  </Trans>
-                </p>
-              </div>
-              <div>
-                <p>
-                  <strong>Github:</strong>
-                  <a
-                    href="https://luka201011.github.io/Website-Taschenrechner/"
-                    id="Github-Taschenrechner"
-                    target="_blank"
-                    className="underline"
-                  >
-                    <Trans>Taschenrechner</Trans>
-                  </a>
-                </p>
-              </div>
-            </div>
-            <div
-              className="btn-2 bg-btn-back text-white mt-2 justify-self-center text-center"
-              onClick={() => handleFlip("minion")}
-            >
+              <p className="mb-1">
+                <strong>Portfolio-Website:</strong>{" "}
+                <Trans>Ich habe eine Portfolio Website erstellt.</Trans>
+              </p>
+              <p className="mb-1">
+                <Trans>
+                  <strong>Taschenrechner:</strong> In einer Gruppenarbeit
+                  entwickelte ich mit meinen Teamkollegen einen einfachen
+                  Taschenrechner als Web-Applikation. Dabei übernahm ich die
+                  Implementierung der Grundfunktionen wie Addition, Subtraktion,
+                  Multiplikation und Division.
+                </Trans>
+              </p>
               <p>
-                <Trans>zurück</Trans>
+                <strong>Github:</strong>{" "}
+                <a
+                  href="https://luka201011.github.io/Website-Taschenrechner/"
+                  id="Github-Taschenrechner"
+                  target="_blank"
+                  className="underline text-second"
+                >
+                  <Trans>Taschenrechner</Trans>
+                </a>
               </p>
             </div>
+            <button
+              className="btn-2 bg-btn-back text-white mt-4 justify-self-center text-center"
+              onClick={() => handleFlip("minion")}
+            >
+              <Trans>zurück</Trans>
+            </button>
           </div>
         </div>
 
+        {/* Dritte Karte: Apps Team */}
         <div
-          className={`card-body-project text-center mt-10 p-10 h-[450px] xl:h-[320px] md:h-[320] ${activeCard === "apps" ? "flipped" : ""}`}
+          className={`card-body-project text-center mt-10 h-[450px] xl:h-[320px] md:h-[320px] ${activeCard === "apps" ? "flipped" : ""}`}
         >
-          <div className="front-card-project bg-card-bg-white">
+          <div className="front-card-project bg-card-bg-white flex-col">
             <div>
               <h2 className="text-lg font-bold">Appes Team (Frontend)</h2>
             </div>
@@ -142,7 +142,7 @@ export default function ProjectsSection() {
               </p>
             </div>
             <div>
-              <p className="mt-2">
+              <p className="mt-2 text-sm md:text-base">
                 <Trans>
                   Das Apps Team gab mir einen praxisnahen Einblick in die
                   Entwicklung moderner Webapplikationen. Mit Technologien wie
@@ -154,13 +154,14 @@ export default function ProjectsSection() {
                 </Trans>
               </p>
               <button
-                className="btn bg-second text-white justify-self-center mt-8"
+                className="btn bg-second hover:bg-btn-hover text-white justify-self-center mt-4"
                 onClick={() => handleFlip("apps")}
               >
                 <Trans>Mehr Infos</Trans>
               </button>
             </div>
           </div>
+
           <div className="back-card-project flex-col bg-card-bg-white">
             <div>
               <h2 className="text-lg font-bold">
@@ -168,25 +169,21 @@ export default function ProjectsSection() {
               </h2>
             </div>
             <div className="title-projects-line bg-second"></div>
-            <div className="m-2">
-              <p className="mb-2">
+            <div className="m-2 text-sm">
+              <p className="mb-2 font-bold">
                 <Trans>Das konnte ich alles umsetzten:</Trans>
               </p>
-              <div>
-                <p>
-                  <strong>Portfolio-Website:</strong>
-                  <Trans>Ich habe diese Portfolio Website erstellt.</Trans>
-                </p>
-              </div>
-            </div>
-            <div
-              className="btn-2 bg-btn-back text-white mt-2 flex justify-center items-center text-center"
-              onClick={() => handleFlip("apps")}
-            >
               <p>
-                <Trans>zurück</Trans>
+                <strong>Portfolio-Website:</strong>{" "}
+                <Trans>Ich habe diese Portfolio Website erstellt.</Trans>
               </p>
             </div>
+            <button
+              className="btn-2 bg-btn-back text-white mt-8 flex justify-center items-center text-center"
+              onClick={() => handleFlip("apps")}
+            >
+              <Trans>zurück</Trans>
+            </button>
           </div>
         </div>
       </div>
